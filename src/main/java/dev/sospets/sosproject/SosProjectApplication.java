004677886a0b9232da853d3dfdf200e1d247d774
ViniciusSotana <vinysotana@gmail.com>
2025-10-08 23:09:41 -0300

    initial commit

---
+++ src/main/java/dev/sospets/sosproject/SosProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.sospets.sosproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SosProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/sospets/sosproject/SosProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sospets.sosproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
