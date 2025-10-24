f5bd564cba1422d4611cf624816e04ddf4d1e2f9
diego brito <diegobritosilva17@gmail.com>
2025-10-24 20:51:39 -0300

    teste de funcionalidade do git com o projeto

--- src/main/java/dev/sospets/sosproject/Post/PostController.java
+++ src/main/java/dev/sospets/sosproject/Post/PostController.java
@@ -1,4 +1,17 @@
 package dev.sospets.sosproject.Post;
 
+import dev.sospets.sosproject.User.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {this.postService = postService;}
+
 }
